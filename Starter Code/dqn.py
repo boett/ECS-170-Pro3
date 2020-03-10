3879b487beb1ebaf899dd414064738e3f2f6baef
Brandon Boettcher <btboettcher@ucdavis.edu>
2020-03-09 19:59:35 -0700

    learning rate

--- Starter Code/dqn.py
+++ Starter Code/dqn.py
@@ -71,7 +71,7 @@ def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer): # co
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
     state = Variable(torch.FloatTensor(np.float32(state)))
-    next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad = True)
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)))
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = Variable(torch.FloatTensor(done))
